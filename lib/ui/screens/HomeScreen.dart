b8a825fe7bea3b86c18a1df8582a1fb727c0f5f3
G4 <yessam770@gmail.com>
2024-06-25 21:45:11 +0300

    Initial coomit .

---
+++ lib/ui/screens/HomeScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeSceen extends StatelessWidget {
+  static const String routeName = 'homeScreen';
+  const HomeSceen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
